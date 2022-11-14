c0b342891c72355192824b22ddf9cc6d2f4a9e6f
Wanderson Felipe <wandersonfelipeplus13@gmail.com>
2022-11-14 01:39:03 -0300

    Dynamic properties in Big Card

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
-  styleUrls: ['./big-card.component.css']
+  styleUrls: ['./big-card.component.css'],
 })
 export class BigCardComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  photoCover: string = 'https://source.unsplash.com/random/?news';
+
+  @Input()
+  cardTitle: string = 'Big News Title';
+
+  @Input()
+  cardDescription = 'Big News Description'
+
+  constructor() {}
 
   ngOnInit(): void {
+    this.cardTitle = this.cardTitle.toLocaleUpperCase()
   }
 
 }
